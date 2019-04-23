6ed56f323b096d8392ac1dcb8b328a67256edbb9
boris <boris.vladt@gmail.com>
2019-04-23 14:29:11 +0200

    Mocking for test countdown function

--- 09_mocking/countdown.go
+++ 09_mocking/countdown.go
@@ -1,5 +1,43 @@
 package main
 
-func Main() {
-	Countdown()
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type MockSleeper struct {
+	Calls int
+}
+
+func (s *MockSleeper) Sleep() {
+	s.Calls++
+}
+
+type RealSleeper struct {}
+
+func (d *RealSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const countdownStart = 3
+const final = "Go!"
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, final)
+}
+
+func main() {
+	sleeper := &RealSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- 09_mocking/countdown_test.go
+++ 09_mocking/countdown_test.go
@@ -7,14 +7,21 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &MockSleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
-	want := "3"
+	want := `3
+2
+1
+Go!`
 
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
 	}
-}
 
+	if spySleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+	}
+}
